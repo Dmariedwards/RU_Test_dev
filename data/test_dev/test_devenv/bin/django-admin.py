3ed42cdd3747a2cabeb8258100baaad79c39ef02
Marie <dmariedwards@aol.com>
2016-10-16 18:38:20 -0400

    updates

---
+++ data/test_dev/test_devenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moon/Desktop/ru-test-environment/data/test_dev/test_devenv/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
