099cd71f9e0d5ccc2f67571283cb5f430e28defe
Marie <dmariedwards@aol.com>
2016-10-17 02:21:16 -0400

    10/17

--- data/test_dev/ru_test/ru_inv/models.py
+++ data/test_dev/ru_test/ru_inv/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Computer(models.Model):
+	Manufacturer = models.CharField(max_length = 250)
+	Serial = models.IntegerField()
+	Comments = models.CharField(max_length = 1000)
